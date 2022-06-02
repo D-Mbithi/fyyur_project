2f18852527ecada4418c9e64a727c45dac8f3206
dennis mbithi <denmbithi@gmail.com>
2022-06-02 10:08:51 +0300

    Implemented edit function for venues and artists

--- app.py
+++ app.py
@@ -499,24 +499,30 @@ def edit_artist(artist_id):
 
 @app.route("/artists/<int:artist_id>/edit", methods=["POST"])
 def edit_artist_submission(artist_id):
-    # TODO: take values from the form submitted, and update existing
+    # DONE TODO: take values from the form submitted, and update existing
     # artist record with ID <artist_id> using the new attributes
     form = ArtistForm()
 
-    artist = Artist(
-        name = form.name.data,
-        city = form.city.data,
-        state = form.state.data,
-        phone = form.phone.data,
-        image_link = form.image_link.data,
-        genres = form.genres.data,
-        facebook_link = form.facebook_link.data,
-        website = form.website_link.data,
-        seeking_venue = form.seeking_venue.data,
-        seeking_description = form.seeking_description.data
-    )
+    artist = Artist.query.get_or_404(artist_id)
+
+    artist.name = form.name.data,
+    artist.city = form.city.data,
+    artist.state = form.state.data,
+    artist.phone = form.phone.data,
+    artist.image_link = form.image_link.data,
+    artist.genres = form.genres.data,
+    artist.facebook_link = form.facebook_link.data,
+    artist.website = form.website_link.data,
+    artist.seeking_venue = form.seeking_venue.data,
+    artist.seeking_description = form.seeking_description.data
+
+    if form.seeking_venue.data:
+        artist.seeking_venue = True
+    else:
+        artist.seeking_venue = False
+
+
     try:
-        db.session.update(artist)
         db.session.commit()
         flash("Artist " + form.name.data + " was successfully updated!")
     except:
@@ -548,12 +554,41 @@ def edit_venue(venue_id):
     # }
     # DONE TODO: populate form with values from venue with ID <venue_id>
     venue = Venue.query.get_or_404(venue_id)
-    form = VenueForm(venue)
+    form = VenueForm(obj=venue)
+
     return render_template("forms/edit_venue.html", form=form, venue=venue)
 
 @app.route("/venues/<int:venue_id>/edit", methods=["POST"])
 def edit_venue_submission(venue_id):
-    # TODO: take values from the form submitted, and update existing
+    form = VenueForm()
+    venue = Venue.query.get_or_404(venue_id)
+
+    venue.name = form.name.data,
+    venue.city = form.city.data,
+    venue.state = form.state.data,
+    venue.address = form.address.data,
+    venue.phone = form.phone.data,
+    venue.genres = form.genres.data,
+    venue.facebook_link = form.facebook_link.data,
+    venue.website = form.website_link.data,
+    venue.image_link = form.image_link.data,
+    venue.seeking_talent = form.seeking_talent.data,
+    venue.seeking_description = form.seeking_description.data
+
+    if form.seeking_talent.data:
+        venue.seeking_talent = True
+    else:
+        venue.seeking_talent = False
+
+    try:
+        db.session.commit()
+        flash("Venue " + form.name.data + " was successfully updated!")
+    except:
+        db.session.rollback()
+        flash('An error occurred. Artist ' + form.name.data + ' could not be updated.')
+    finally:
+        db.session.close()
+    # DONE TODO: take values from the form submitted, and update existing
     # venue record with ID <venue_id> using the new attributes
     return redirect(url_for("show_venue", venue_id=venue_id))
 
