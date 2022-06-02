10911990033599a96ca94e6d30ea77991ad3e4ae
dennis <denmbithi@gmail.com>
2022-06-02 21:15:29 +0300

    Model logic separation form app

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
 from flask_migrate import Migrate
+from .models import Artist, Show, Venue
 
 # ----------------------------------------------------------------------------#
 # App Config.
@@ -26,60 +27,6 @@ migrate = Migrate(app, db)
 
 # DONE TODO: connect to a local postgresql database
 
-# ----------------------------------------------------------------------------#
-# Models.
-# ----------------------------------------------------------------------------#
-
-
-class Venue(db.Model):
-    __tablename__ = "Venue"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    genres = db.Column(db.String(120))
-    website = db.Column(db.String())
-    facebook_link = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean(), default=False)
-    seeking_description = db.Column(db.String)
-    show = db.relationship("Show", backref="venue", lazy=True)
-
-    def __repr__(self):
-        return f"{self.id}. {self.name}"
-
-    # Done TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-class Artist(db.Model):
-    __tablename__ = "Artist"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    website = db.Column(db.String(120))
-    facebook_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean(), default=False)
-    seeking_description = db.Column(db.String())
-    show = db.relationship("Show", backref="artist", lazy=True)
-
-    def __repr__(self):
-        return f"{self.id}. {self.name}"
-
-    # Done TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-class Show(db.Model):
-    __tabelanem__ = "Show"
-    id = db.Column(db.Integer, primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey("Venue.id"))
-    artist_id = db.Column(db.Integer, db.ForeignKey("Artist.id"))
-    start_time = db.Column(db.String)
 
 # DONE TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
@@ -112,31 +59,31 @@ def index():
 def venues():
     # DONE TODO: replace with real venues data.
     #       num_upcoming_shows should be aggregated based on number of upcoming shows per venue.
-    # data=[{
-    #   "city": "San Francisco",
-    #   "state": "CA",
-    #   "venues": [{
-    #     "id": 1,
-    #     "name": "The Musical Hop",
-    #     "num_upcoming_shows": 0,
-    #   }, {
-    #     "id": 3,
-    #     "name": "Park Square Live Music & Coffee",
-    #     "num_upcoming_shows": 1,
-    #   }]
-    # }, {
-    #   "city": "New York",
-    #   "state": "NY",
-    #   "venues": [{
-    #     "id": 2,
-    #     "name": "The Dueling Pianos Bar",
-    #     "num_upcoming_shows": 0,
-    #   }]
-    # }]
+    data=[{
+      "city": "San Francisco",
+      "state": "CA",
+      "venues": [{
+        "id": 1,
+        "name": "The Musical Hop",
+        "num_upcoming_shows": 0,
+      }, {
+        "id": 3,
+        "name": "Park Square Live Music & Coffee",
+        "num_upcoming_shows": 1,
+      }]
+    }, {
+      "city": "New York",
+      "state": "NY",
+      "venues": [{
+        "id": 2,
+        "name": "The Dueling Pianos Bar",
+        "num_upcoming_shows": 0,
+      }]
+    }]
 
     venues = Venue.query.all()
 
-    return render_template("pages/venues.html", areas=venues)
+    return render_template("pages/venues.html", areas=data)
 
 @app.route("/venues/search", methods=["POST"])
 def search_venues():
---
+++ models.py
@@ -0,0 +1,54 @@
+from .app import db
+# ----------------------------------------------------------------------------#
+# Models.
+# ----------------------------------------------------------------------------#
+
+class Venue(db.Model):
+    __tablename__ = "Venue"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    genres = db.Column(db.String(120))
+    website = db.Column(db.String())
+    facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean(), default=False)
+    seeking_description = db.Column(db.String)
+    show = db.relationship("Show", backref="venue", lazy=True)
+
+    def __repr__(self):
+        return f"{self.id}. {self.name}"
+
+    # Done TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+class Artist(db.Model):
+    __tablename__ = "Artist"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean(), default=False)
+    seeking_description = db.Column(db.String())
+    show = db.relationship("Show", backref="artist", lazy=True)
+
+    def __repr__(self):
+        return f"{self.id}. {self.name}"
+
+    # Done TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+class Show(db.Model):
+    __tabelanem__ = "Show"
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey("Venue.id"))
+    artist_id = db.Column(db.Integer, db.ForeignKey("Artist.id"))
+    start_time = db.Column(db.String)
